e3e5dbd6d56e380e2100a0fc75c7405a2a14e6bc
Evan Moritz <emoritzx@gmail.com>
2019-03-05 21:04:49 -0500

    Added non-platform constructor to Tile and improved serialization

--- lanterns-core/src/main/java/me/abiogenesis/lanterns/Tile.java
+++ lanterns-core/src/main/java/me/abiogenesis/lanterns/Tile.java
@@ -8,9 +8,13 @@ public class Tile {
 
     private final Map<Direction, Lantern> lanterns = new LinkedHashMap<>();
     private final Map<Direction, Tile> edges = new LinkedHashMap<>();
-    private final Optional<String> platform;
+    private final String platform;
 
-    public Tile(Lantern north, Lantern east, Lantern south, Lantern west, Optional<String> platform) {
+    public Tile(Lantern north, Lantern east, Lantern south, Lantern west) {
+        this(north, east, south, west, null);
+    }
+
+    public Tile(Lantern north, Lantern east, Lantern south, Lantern west, String platform) {
         lanterns.put(NORTH, north);
         lanterns.put(EAST, east);
         lanterns.put(SOUTH, south);
@@ -23,7 +27,7 @@ public class Tile {
     }
 
     public Optional<String> getPlatform() {
-        return platform;
+        return Optional.ofNullable(platform);
     }
 
     public Optional<Tile> getEdge(Direction direction) {
@@ -37,5 +41,18 @@ public class Tile {
     public void clear() {
         Arrays.stream(Direction.values())
             .forEach(this::removeEdge);
+    }
+
+    public void rotate() {
+        if (edges.isEmpty()) {
+            Direction currentDirection = NORTH;
+            do {
+                Lantern currentLantern = lanterns.get(currentDirection);
+                currentDirection.clockwise();
+                lanterns.put(currentDirection, currentLantern);
+            } while (currentDirection != NORTH);
+        } else {
+            throw new IllegalStateException("Cannot rotate a tile with edges");
+        }
     }
 }
